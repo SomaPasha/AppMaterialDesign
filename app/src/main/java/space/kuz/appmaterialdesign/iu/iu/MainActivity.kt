baafcbfd08398294e30f26e6357e9ec008dcdf5e
Ferpasha <pavel.kuzneczov.96@gmail.com>
2021-12-15 22:43:49 +0500

    Запуск и загрузка картинки

--- app/src/main/java/space/kuz/appmaterialdesign/iu/iu/MainActivity.kt
+++ app/src/main/java/space/kuz/appmaterialdesign/iu/iu/MainActivity.kt
@@ -2,11 +2,28 @@ package space.kuz.appmaterialdesign.iu.iu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import space.kuz.appmaterialdesign.R
+import space.kuz.appmaterialdesign.databinding.ActivityMainBinding
+import space.kuz.appmaterialdesign.iu.iu.fragment.DailyImageFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+lateinit var  binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fragment  = DailyImageFragment()
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragment_daily, fragment)
+        transaction.disallowAddToBackStack()
+        transaction.commit()
+
+
+
+
     }
 }
--- app/src/main/java/space/kuz/appmaterialdesign/iu/iu/fragment/DailyImageFragment.kt
+++ app/src/main/java/space/kuz/appmaterialdesign/iu/iu/fragment/DailyImageFragment.kt
@@ -1,7 +1,14 @@
 package space.kuz.appmaterialdesign.iu.iu.fragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import coil.api.load
+import space.kuz.appmaterialdesign.R
 import space.kuz.appmaterialdesign.domain.entity.DailyImage
 import space.kuz.appmaterialdesign.iu.iu.viewmodel.DailyImageViewModel
 
@@ -9,5 +16,49 @@ class DailyImageFragment: Fragment() {
 
  private val viewModel by viewModels<DailyImageViewModel>()
 
+ private lateinit var  dailyImageView: ImageView
 
+ override fun onCreate(savedInstanceState: Bundle?) {
+  super.onCreate(savedInstanceState)
+
+  viewModel.getImageData().observe(this,{dailyImage->renderData(dailyImage)})
+ }
+
+ override fun onCreateView(
+  inflater: LayoutInflater,
+  container: ViewGroup?,
+  savedInstanceState: Bundle?
+ ): View? {
+  return inflater.inflate(R.layout.fragment_daily_image,container,false)
+ }
+ override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+  super.onViewCreated(view, savedInstanceState)
+  dailyImageView = view.findViewById(R.id.image_view_nasa_image)
+ }
+
+
+ private  fun renderData(dailyImage: DailyImage){
+  when(dailyImage){
+   is DailyImage.Success->{
+    val serverResponseData =  dailyImage.serverResponseData
+    val url = serverResponseData.url
+
+    if (url.isNullOrEmpty()){
+
+    } else{
+     dailyImageView.load(url){
+      lifecycle(this@DailyImageFragment)
+      error(R.drawable.ic_error)
+      placeholder(R.drawable.ic_placeholder)
+     }
+    }
+   }
+   is DailyImage.Loading->{
+
+   }
+   is DailyImage.Error->{
+
+   }
+  }
+ }
 }
