6769c298325283a5bf1f499c620b34e6329f43cc
Ferpasha <pavel.kuzneczov.96@gmail.com>
2021-12-16 07:38:43 +0500

    Добавление BottomSheet

--- app/src/main/java/space/kuz/appmaterialdesign/iu/iu/fragment/DailyImageFragment.kt
+++ app/src/main/java/space/kuz/appmaterialdesign/iu/iu/fragment/DailyImageFragment.kt
@@ -7,9 +7,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import coil.api.load
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import space.kuz.appmaterialdesign.R
 import space.kuz.appmaterialdesign.databinding.FragmentDailyImageBinding
 import space.kuz.appmaterialdesign.domain.entity.DailyImage
@@ -20,11 +24,15 @@ class DailyImageFragment: Fragment() {
  private val viewModel by viewModels<DailyImageViewModel>()
  private  lateinit var binding: FragmentDailyImageBinding
  private lateinit var  dailyImageView: ImageView
+ private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+ private lateinit var bottomSheetDescription: TextView
+ private lateinit var bottomSheetDescriptionHeader: TextView
 
  override fun onCreate(savedInstanceState: Bundle?) {
   super.onCreate(savedInstanceState)
 
   viewModel.getImageData().observe(this,{dailyImage->renderData(dailyImage)})
+
  }
 
  override fun onCreateView(
@@ -39,22 +47,48 @@ class DailyImageFragment: Fragment() {
  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
   super.onViewCreated(view, savedInstanceState)
   dailyImageView = view.findViewById(R.id.image_view_nasa_image)
+  bottomSheetDescription = view.findViewById(R.id.bottom_sheet_description)
+  bottomSheetDescriptionHeader =view.findViewById(R.id.bottom_sheet_description_header)
   binding.inputLayoutWiki.setEndIconOnClickListener {
    val intent = Intent(Intent.ACTION_VIEW)
    val url = "https://en.wikipedia.org/wiki/${ binding.inputEditTextWiki.text.toString()}"
    val uri = Uri.parse(url)
    intent.data = uri
-
    startActivity(intent)
   }
-
+  startBottomSheetBehavior(view)
  }
 
+ private  fun startBottomSheetBehavior(view: View){
+  setBottomSheetBehavior(view.findViewById(R.id.bottom_sheet_container))
+  bottomSheetBehavior.addBottomSheetCallback(object :
+   BottomSheetBehavior.BottomSheetCallback() {
+   override fun onStateChanged(bottomSheet: View, newState: Int) {
+    when (newState) {
 
+     BottomSheetBehavior.STATE_DRAGGING ->  Toast.makeText(context,"STATE_DRAGGING",Toast.LENGTH_SHORT).show()
+     BottomSheetBehavior.STATE_COLLAPSED ->  Toast.makeText(context,"STATE_COLLAPSED",Toast.LENGTH_SHORT).show()
+     BottomSheetBehavior.STATE_EXPANDED ->  Toast.makeText(context,"STATE_EXPANDED",Toast.LENGTH_SHORT).show()
+     BottomSheetBehavior.STATE_HALF_EXPANDED ->  Toast.makeText(context,"STATE_HALF_EXPANDED",Toast.LENGTH_SHORT).show()
+     BottomSheetBehavior.STATE_HIDDEN ->  Toast.makeText(context,"STATE_HIDDEN",Toast.LENGTH_SHORT).show()
+     BottomSheetBehavior.STATE_SETTLING ->  Toast.makeText(context,"STATE_SETTLING",Toast.LENGTH_SHORT).show()
+    }
+   }
+
+   override fun onSlide(bottomSheet: View, slideOffset: Float) {
+   }
+  })
+
+ }
  private  fun renderData(dailyImage: DailyImage){
   when(dailyImage){
    is DailyImage.Success->{
+
     val serverResponseData =  dailyImage.serverResponseData
+    val planation = serverResponseData.explanation
+    val planationHead = serverResponseData.title
+    bottomSheetDescription.text = planation
+    bottomSheetDescriptionHeader.text= planationHead
     val url = serverResponseData.url
 
     if (url.isNullOrEmpty()){
@@ -75,4 +109,15 @@ class DailyImageFragment: Fragment() {
    }
   }
  }
+ private fun setBottomSheetBehavior(bottomSheet: ConstraintLayout) {
+  bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+  bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
+ }
+
+
+
+
+
+
+
 }
