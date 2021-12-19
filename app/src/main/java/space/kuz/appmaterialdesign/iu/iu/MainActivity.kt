4150926913aeacd30006a8d30142a1a4de59a3e0
Ferpasha <pavel.kuzneczov.96@gmail.com>
2021-12-19 17:40:29 +0500

    Создание автоматической тёмной темы

--- app/src/main/java/space/kuz/appmaterialdesign/iu/iu/MainActivity.kt
+++ app/src/main/java/space/kuz/appmaterialdesign/iu/iu/MainActivity.kt
@@ -1,14 +1,19 @@
 package space.kuz.appmaterialdesign.iu.iu
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import space.kuz.appmaterialdesign.R
 import space.kuz.appmaterialdesign.databinding.ActivityMainBinding
 import space.kuz.appmaterialdesign.iu.iu.fragment.DailyImageFragment
 
 class MainActivity : AppCompatActivity(){
+
 lateinit var  binding: ActivityMainBinding
+    @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO)
         setTheme(Const.currentTheme);
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
