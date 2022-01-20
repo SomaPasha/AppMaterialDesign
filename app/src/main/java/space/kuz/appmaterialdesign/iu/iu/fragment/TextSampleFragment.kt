a45d61094831f4bd824ed753fc74d03aa0d59f04
Ferpasha <pavel.kuzneczov.96@gmail.com>
2022-01-20 15:55:08 +0500

    Спаны с урока

--- app/src/main/java/space/kuz/appmaterialdesign/iu/iu/fragment/TextSampleFragment.kt
+++ app/src/main/java/space/kuz/appmaterialdesign/iu/iu/fragment/TextSampleFragment.kt
@@ -1,17 +1,54 @@
 package space.kuz.appmaterialdesign.iu.iu.fragment
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.method.LinkMovementMethod
 import android.view.*
-import androidx.fragment.app.Fragment
+import android.widget.*
+import androidx.fragment.app.*
 import space.kuz.appmaterialdesign.R
+import space.kuz.appmaterialdesign.iu.iu.viewmodel.TextSampleViewModel
+import java.lang.IllegalArgumentException
 
 class TextSampleFragment:Fragment() {
+    private val viewModel by viewModels<TextSampleViewModel>()
 
+    private lateinit var spannableSampleTextView: TextView
+    private lateinit var radioGroup: RadioGroup
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_text_sample, container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        spannableSampleTextView = view.findViewById(R.id.text_view_spanned_text)
+        radioGroup = view.findViewById(R.id.radio_group_span)
+
+        viewModel.getSpannableText().observe(viewLifecycleOwner){text ->
+            spannableSampleTextView.text = text
+        }
+
+        viewModel.getMessageText().observe(viewLifecycleOwner){text ->
+           Toast.makeText(requireContext(),text,Toast.LENGTH_SHORT).show()
+        }
+
+        radioGroup.setOnCheckedChangeListener { group, checkedId ->
+            when(checkedId){
+                R.id.radio_button_background_color_span -> viewModel.onBackgroundColorSpanClicked()
+                R.id.radio_button_clickable_span -> viewModel.onClickableSpanClicked()
+                R.id.radio_button_image_span-> viewModel.onImageSpanClicked(requireContext())
+                else -> throw IllegalArgumentException("unknow id")
+            }
+        }
+
+        spannableSampleTextView.movementMethod = LinkMovementMethod.getInstance()
+
+        val radioButton:RadioButton = view.findViewById(R.id.radio_button_clickable_span)
+        radioButton.isChecked = true
     }
 }
---
+++ app/src/main/java/space/kuz/appmaterialdesign/iu/iu/viewmodel/TextSampleViewModel.kt
@@ -0,0 +1,53 @@
+package space.kuz.appmaterialdesign.iu.iu.viewmodel
+
+import android.content.Context
+import android.graphics.Color
+import android.text.Spanned
+import android.text.style.*
+import android.view.View
+import androidx.core.text.toSpannable
+import androidx.lifecycle.*
+import space.kuz.appmaterialdesign.R
+
+private  const val initialTextForSpan = "It is text for span. * for image Span"
+
+class TextSampleViewModel :ViewModel() {
+
+    private val spannableTextLiveData = MutableLiveData<CharSequence>()
+    private val messageTextLiveData = MutableLiveData<String>()
+
+    fun getSpannableText():LiveData<CharSequence>{
+        return  spannableTextLiveData
+    }
+    fun getMessageText():LiveData<String>{
+        return  messageTextLiveData
+    }
+
+    fun onBackgroundColorSpanClicked(){
+        val spannable = initialTextForSpan.toSpannable()
+        spannable.setSpan(BackgroundColorSpan(Color.CYAN),3,5,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableTextLiveData.value = spannable
+    }
+
+    fun onClickableSpanClicked(){
+        val clickableSpan = object :ClickableSpan(){
+            override fun onClick(widget: View) {
+              messageTextLiveData.value = "Span Clicked"
+            }
+
+        }
+        val spannable = initialTextForSpan.toSpannable()
+        spannable.setSpan(clickableSpan, 3,5,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableTextLiveData.value = spannable
+    }
+
+    fun onImageSpanClicked (context:Context){
+        val imageSpan = ImageSpan(context, R.drawable.fly_image)
+
+        val spannable = initialTextForSpan.toSpannable()
+        spannable.setSpan(imageSpan, 3,5,Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        spannableTextLiveData.value = spannable
+    }
+
+
+}
